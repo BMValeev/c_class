5283ea653266a4b3ca224a54ec70de99dc53f5e7
BMValeev <BMValeev@stud.kai.ru>
2018-12-18 08:23:51 +0300

    Добавлена проверка на случай если длина ответа ==0 или ==ff

--- I2C.cpp
+++ I2C.cpp
@@ -155,6 +155,10 @@ int I2C::SendRaw_new(std::vector<unsigned char> address, std::vector<unsigned ch
     {
         this->LastRecMsg.push_back(buf_rec[cnt]);
     }
+    if((this->LastRecMsg.front()==0x00)|(this->LastRecMsg.front()==0xff))
+    {
+        return NOK;
+    }
     cnt_all=cnt_all-this->LastRecMsg.front();
     while (--cnt_all)
     {
